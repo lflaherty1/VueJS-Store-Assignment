d8743f32ac4d9aba5acd9ebce4d62ee3c73eb149
Luke Flaherty <lflaherty@deloitte.com>
2022-10-19 08:39:01 -0400

    committing bc its time for demo

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -3,28 +3,37 @@
         <p>Your Cart</p>
         <p>No. of items: {{cart.length}}</p>
         <p>Total: ${{total}}</p>
+        <p>Items: </p>
+        <!-- <p> {{if(cart.length > 0{
+            while (i < cart.length){
+                `<p>{{total}}</p>`
+            }
+        })}}</p> -->
+        <div v-for="item in cart" 
+            :key="item.key"
+            :item="item">
+            {{cart[item]}}
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'shopping-cart',
-    components: {},
-    props: ['cart', 'total'],
-    computed: {
-
-    }
-}
+  name: "shopping-cart",
+  components: {},
+  props: ["cart", "total"],
+  computed: {},
+};
 </script>
 
 <style>
 p {
-    margin: 0;
+  margin: 0;
 }
 
 #ShoppingCart {
-    padding: 10px;
-    border-radius: 5px;
-    background: #494949;
+  padding: 10px;
+  border-radius: 5px;
+  background: #494949;
 }
 </style>
